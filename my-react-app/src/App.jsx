398f409dd7353841d9f58904c59f216933c345d2
Scrum-master-developer <canalotakurafael2003@gmail.com>
2026-08-14 00:07:20 -0300

    Path: O código foi organizado, separando as rotas /Home e /App, adicionado botão para seção de orçamento

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,38 @@
 import './App.css'
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './assets/logo.png';
 import Container from './containments/container';
 import ContainerServicos from './containments/container-servicos'
 import { Instagram, Whatsapp } from 'react-bootstrap-icons';
 
-import Orcamento from './pages/orcamento.jsx'
+import Orcamento from './pages/orcamento.jsx';
 
 const loja = {
   nome: 'Phantom System',
   logo: logo,
   contato: '14 99889-2226'
 };
+
+function Home() {
+  return(
+    <>
+      <div className='App'>
+        <img src={loja.logo} alt={`Logo da loja ${loja.nome}`} className="logo" />
+        <h1 className='nome-da-loja'>{loja.nome}</h1>
+        <p className='p-principal'>Seu computador está com problemas?<br />Nós temos a solução!</p>
+      </div>
+
+      <main className='conteudo-principal'>
+          {/* Lista de serviços */}
+          <section>
+            <Container />
+            <ContainerServicos />
+          </section>
+      </main>
+    </>
+  );
+}
 
 function App() {
 
@@ -22,51 +42,40 @@ function App() {
 
   return (
     <div>
-      {/* Menu de navegação da página */}
       <nav>
-
         {/* Nome da Loja */}
         <span className="nomeLoja">{loja.nome}</span>
-        
-        <Switch>
-          <Route exact path='/Orcamento' Component={Orcamento}></Route>
-        </Switch>
-        
+
+        <Link to="/Orcamento" className="btn btn-primary" >
+          Orçamento
+        </Link>
 
         {/* Ícones das Redes Sociais */}
         <div>
           <a href="https://www.instagram.com/" className='link'>
-            <Instagram size={30} className='icon' color='#8182b4' />
+            <Instagram 
+              size={30}
+              className='icon' 
+              color='#8182b4' 
+            />
           </a>
         </div>
         <div>
-          <Whatsapp size={30} className='icon' color='#8182b4' onClick={mostrarNumero} style={{ cursor: 'pointer' }} />
+          <Whatsapp   
+            size={30}
+            className='icon' 
+            color='#8182b4' 
+            onClick={mostrarNumero} 
+            style={{ cursor: 'pointer' }} 
+          />
         </div>
-
       </nav>
 
-      <div className='App'>
-        {/* 2. CORREÇÃO: Alterado de loja.name para loja.nome */}
-        <img src={loja.logo} alt={`Logo da loja ${loja.nome}`} className="logo" />
-
-        {/* 3. CORREÇÃO: Alterado de loja.name para loja.nome */}
-        <h1 className='nome-da-loja'>{loja.nome}</h1>
-        
-        <p className='p-principal'>
-          Seu computador está com problemas?<br />
-          Nós temos a solução!
-        </p>
-      </div>
-
-      {/* Seção principal */}
-      <main className='conteudo-principal'>
-        {/* Lista de serviços */}
-        <section>
-          <Container />
-          <ContainerServicos />
-        </section>
-
-      </main>
+      {/* Gerenciador de rotas*/}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Orcamento" element={<Orcamento/>} />
+      </Routes>
 
       <footer className='footer'>
         <p className='contatos'>
---
+++ my-react-app/src/pages/orcamento.jsx
@@ -0,0 +1,11 @@
+import Container from "../containments/container";
+
+function Orcamento() {
+    return(
+        <>
+            <Container />
+        </>
+    );
+}
+
+export default Orcamento;
